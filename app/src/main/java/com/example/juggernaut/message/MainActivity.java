2e97367e327b7462fe0f9b4bceff2b9bbd75f99b
ashu94 <singhjug1994@gmail.com>
2016-02-11 04:01:29 +0530

    layout created

--- app/src/main/java/com/example/juggernaut/message/MainActivity.java
+++ app/src/main/java/com/example/juggernaut/message/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.juggernaut.message;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+/*
+    public void refreshSmsInbox() {
+        ContentResolver contentResolver = getContentResolver();
+        Cursor smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
+        int indexBody = smsInboxCursor.getColumnIndex("body");
+        int indexAddress = smsInboxCursor.getColumnIndex("address");
+        long timeMillis = smsInboxCursor.getColumnIndex("date");
+        Date date = new Date(timeMillis);
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yy");
+        String dateText = format.format(date);
+
+        if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
+        arrayAdapter.clear();
+        do {
+            String str = smsInboxCursor.getString(indexAddress) +" at "+
+                    "\n" + smsInboxCursor.getString(indexBody) +dateText+ "\n";
+            arrayAdapter.add(str);
+        } while (smsInboxCursor.moveToNext());
+    }
+*/
 }
